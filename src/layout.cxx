e393404b70d0a498759ce4eadf93f55c51c401ea
gomes <dev.gomesmichel@gmail.com>
2024-11-06 18:03:00 -0300

    iniciando parte de cadastro

--- src/layout.cxx
+++ src/layout.cxx
@@ -39,37 +39,38 @@ Fl_Double_Window* main_window() {
       } // Fl_Button* btn_tela_venda
       { btn_tela_estoque = new Fl_Button(1005, 230, 300, 35, "estoque");
         btn_tela_estoque->callback((Fl_Callback*)show_estoque);
-
       } // Fl_Button* btn_tela_estoque
       tela_menu->end();
     } // Fl_Group* tela_menu
+    
     { tela_menu_venda = new Fl_Group(15, 25, 1330, 695);
       tela_menu_venda->hide();
-      { btn_menu_venda = new Fl_Return_Button(1005, 530, 110, 25, "Voltar");
-        btn_menu_venda->callback((Fl_Callback*)show_menu);
-      } // Fl_Return_Button* btn_menu_venda
-      { btn_nova_venda = new Fl_Button(1005, 330, 300, 35, "Nova venda");
+      { btn_nova_venda = new Fl_Button(1005, 130, 300, 35, "Nova venda");
       } // Fl_Button* btn_nova_venda
       { btn_canc_venda = new Fl_Button(1005, 230, 300, 35, "Cancelar venda");
       } // Fl_Button* btn_canc_venda
-      { btn_visu_vendas = new Fl_Button(1005, 130, 300, 35, "Visualizar vendas");
+      { btn_visu_vendas = new Fl_Button(1005, 330, 300, 35, "Visualizar vendas");
       } // Fl_Button* btn_visu_vendas
+      { btn_menu_venda = new Fl_Return_Button(1005, 530, 110, 25, "Voltar");
+        btn_menu_venda->callback((Fl_Callback*)show_menu);
+      } // Fl_Return_Button* btn_menu_venda
       tela_menu_venda->end();
     } // Fl_Group* tela_menu_venda
+    
     { tela_menu_estoque = new Fl_Group(15, 25, 1330, 695);
       tela_menu_estoque->hide();
+      } // Fl_Return_Button* btn_menu_estoque
+      { cad_prod_estoque = new Fl_Button(1005, 130, 300, 35, "Cadastar Produto");
+      } // Fl_Button* cad_prod_estoque
+      { edt_prod_estoque = new Fl_Button(1005, 230, 300, 35, "Editar Produto");
+      } // Fl_Button* edt_prod_estoque
+      { exc_prod_estoque = new Fl_Button(1005, 330, 300, 35, "Excluir Produto");
+      } // Fl_Button* exc_prod_estoque
       { btn_menu_estoque = new Fl_Return_Button(1005, 530, 110, 25, "Voltar");
         btn_menu_estoque->callback((Fl_Callback*)show_menu);
-
-      } // Fl_Return_Button* btn_menu_estoque
-      { cad_prod_estoque = new Fl_Button(1005, 330, 300, 35, "Cadastar Produto");
-      } // Fl_Button* cad_prod_estoque
-      { exc_prod_estoque = new Fl_Button(1005, 230, 300, 35, "Excluir Produto");
-      } // Fl_Button* exc_prod_estoque
-      { edt_prod_estoque = new Fl_Button(1005, 130, 300, 35, "Editar Produto");
-      } // Fl_Button* edt_prod_estoque
+      } // Fl_Group* tela_menu_estoque
       tela_menu_estoque->end();
-    } // Fl_Group* tela_menu_estoque
+    
     tela_principal->size_range(1364, 739, 1364, 739);
     tela_principal->end();
   } // Fl_Double_Window* tela_principal
